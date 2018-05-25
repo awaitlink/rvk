ec22374cf19bc3c8ae369ddff9e649e0f246be5f
Artem Varaksa <aymfst@gmail.com>
2018-05-25 09:26:00 +0300

    Initial APIClient

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use futures::Future;
+use tokio_core::reactor::Core;
+use hyper::{
+    Client, Body,
+    client::{
+        HttpConnector,
+        FutureResponse,
+    }
+};
+
+pub struct APIClient {
+    api_key: String,
+    client: Client<HttpConnector, Body>,
+    core: Core,
+}
+
+impl APIClient {
+    pub fn new(api_key: String) -> APIClient {
+        let core = Core::new().unwrap();
+
+        APIClient {
+            api_key,
+            client: Client::new(&core.handle()),
+            core,
+        }
+    }
+
+    pub fn run<F: Future>(&mut self, work: F) -> Result<F::Item, F::Error> {
+        self.core.run(work)
+    }
+
+    pub fn request(&self, uri: &str) -> FutureResponse {
+        let uri = uri.parse().unwrap();
+        self.client.get(uri)
     }
 }
