3a69da4af4bae0fab9ca17a36512c7d795b97af7
Artem Varaksa <aymfst@gmail.com>
2018-05-25 20:54:27 +0300

    APIClient: starting parsing response

--- src/client.rs
+++ src/client.rs
@@ -1,14 +1,17 @@
 use std::collections::HashMap;
-use futures::Future;
+
+use futures::{Future, Stream};
 use tokio_core::reactor::Core;
+
 use hyper::{
     Client, Body,
     client::{
         HttpConnector,
-        FutureResponse,
     }
 };
 use hyper_tls::HttpsConnector;
+
+use serde_json::{Value, from_slice};
 
 pub struct APIClient<'a> {
     core: Core,
@@ -37,15 +40,29 @@ impl<'a> APIClient<'a> {
         self.core.run(work)
     }
 
-    pub fn call_method(&self, method_name: &str, params: HashMap<&str, &str>) -> FutureResponse {
+    pub fn call_method<F>(&self, method_name: &str, params: HashMap<&str, &str>, then: F)
+        -> impl Future
+        where F: Fn(APIResponse)
+    {
         let mut url = "https://api.vk.com/method/".to_owned();
-        url += &(method_name.to_owned() + "?v=" + self.api_version + "&token=" + self.token);
+        url += &(method_name.to_owned() + "?v=" + self.api_version + "&access_token=" + self.token);
 
         for (name, value) in params.iter() {
             url += &("&".to_owned() + name + "=" + value);
         }
 
         let url = url.parse().unwrap();
-        self.client.get(url)
+
+        self.client.get(url).and_then(|res| {
+            res.body().concat2().map(move |body| {
+                let data: Value = from_slice(&body).unwrap();
+                then(APIResponse::Ok(data)); // TODO: Distinguish between Ok and Error
+            })
+        })
     }
+}
+
+pub enum APIResponse {
+    Ok(Value),
+    Error(u64, String),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 extern crate futures;
 extern crate tokio_core;
+
 extern crate hyper;
 extern crate hyper_tls;
+
+extern crate serde_json;
 
 pub mod client;
