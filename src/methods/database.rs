9ca852cbd9154a6a9374fe727bcd06c16dfe7111
Artem Varaksa <aymfst@gmail.com>
2019-02-21 16:35:56 +0300

    Added new methods.
    https://vk.com/dev/groups.addAddress
    https://vk.com/dev/groups.editAddress
    https://vk.com/dev/groups.deleteAddress
    https://vk.com/dev/database.getMetroStations
    https://vk.com/dev/database.getMetroStationsById

--- src/methods/database.rs
+++ src/methods/database.rs
@@ -5,6 +5,8 @@ api_category!("database"; methods {
     get_countries,
     get_countries_by_id,
     get_faculties,
+    get_metro_stations,
+    get_metro_stations_by_id,
     get_regions,
     get_school_classes,
     get_schools,
